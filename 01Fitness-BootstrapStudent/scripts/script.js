5fd158971e66cfb2c2345a2e51aea98441436ac1
PalmerCord <82625447+PalmerCord@users.noreply.github.com>
2021-04-27 21:45:38 -0600

    Add files via upload

---
+++ 01Fitness-BootstrapStudent/scripts/script.js
@@ -0,0 +1,12 @@
+/*
+    Student Name:Cord Palmer
+    File Name: script.js
+    Date: 04/27/2021
+*/
+//Jquery for her image to consume the header window space
+$(document) .ready(function(){
+    $('.hero') .height($(window).height());
+});
+
+
+
